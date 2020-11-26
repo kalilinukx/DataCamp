5d5bd3782bc65941a25a3ecbf8c6a3a0ba1397e8
kalilinukx <kalilinuxx007@gmail.com>
2020-11-26 19:00:09 +0530

    Added Raiseval function that raises the value to its given input parameters

---
+++ 03-python-data-science-toolbox-(part-1)/1-writing-your-own-functions/raiseval.py
@@ -0,0 +1,8 @@
+def raise_both(value1, value2):
+    """Raise value1 to the power of value2
+and vice versa."""
+    new_value1 = value1 ** value2
+    new_value2 = value2 ** value1
+    new_tuple = (new_value1, new_value2)
+    return new_tuple
+print(raise_both(12,2))
